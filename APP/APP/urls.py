ee3fd6b3afdd2cab83afd0c5b64f882d5bfc45fe
Alvaro <coarf@hotmail.com>
2021-04-20 21:51:58 -0500

    creacion aplicacion bases

--- APP/APP/urls.py
+++ APP/APP/urls.py
@@ -19,10 +19,10 @@ from django.urls import path, include
 urlpatterns = [
 
     path('',include(('bases.urls','bases'), namespace='bases')),
-    path('inv/',include(('inv.urls','inv'), namespace='inv')),
-    path('cmp/', include(('cmp.urls', 'cmp'), namespace='cmp')),
-    path('fac/', include(('fac.urls', 'fac'), namespace='fac')),
-    path('api/', include(('api.urls', 'api'), namespace='api')),
+    #path('inv/',include(('inv.urls','inv'), namespace='inv')),
+    #path('cmp/', include(('cmp.urls', 'cmp'), namespace='cmp')),
+    #path('fac/', include(('fac.urls', 'fac'), namespace='fac')),
+    #path('api/', include(('api.urls', 'api'), namespace='api')),
     
     path('admin/', admin.site.urls),
 ]
