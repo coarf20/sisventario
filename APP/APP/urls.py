fc5fec7f4f647d1eca27cb2a0e40e2e8cf344d40
Alvaro <coarf@hotmail.com>
2021-04-20 21:22:09 -0500

    agregada las urls

--- APP/APP/urls.py
+++ APP/APP/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+
+    path('',include(('bases.urls','bases'), namespace='bases')),
+    path('inv/',include(('inv.urls','inv'), namespace='inv')),
+    path('cmp/', include(('cmp.urls', 'cmp'), namespace='cmp')),
+    path('fac/', include(('fac.urls', 'fac'), namespace='fac')),
+    path('api/', include(('api.urls', 'api'), namespace='api')),
+    
     path('admin/', admin.site.urls),
 ]
+
